325e60d78b44d8368c181e736943ebaf33ba89cf
Okeanos Viz 1 <viz1@okeanos.net>
2014-02-13 19:03:55 +0200

    Change path for cascedenik library and add systemd script

---
+++ start.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+ 
+NAME="TileStache"                                 # Name of the application
+DIR=/home/user/TileStache                         # Project directory
+SOCKFILE=/home/user/TileStache/tilestache.sock    # we will communicte using this unix socket
+USER=user                                         # the user to run as
+GROUP=user                                        # the group to run as
+NUM_WORKERS=4                                     # how many worker processes should Gunicorn spawn
+ 
+echo "Starting $NAME as `whoami`"
+ 
+# Activate the virtual environment
+# cd $DJANGODIR
+# source ../bin/activate
+ 
+# Create the run directory if it doesn't exist
+RUNDIR=$(dirname $SOCKFILE)
+test -d $RUNDIR || mkdir -p $RUNDIR
+ 
+# Start your Django Unicorn
+# Programs meant to be run under supervisor should not daemonize themselves (do not use --daemon)
+cd $DIR
+exec /usr/bin/gunicorn server:application \
+  --name $NAME \
+  --workers $NUM_WORKERS \
+  --user=$USER --group=$GROUP \
+  --log-level=info \
+  --bind=unix:$SOCKFILE
--- tilestache.wsgi
+++ tilestache.wsgi
@@ -2,11 +2,12 @@ import os
 import sys
 cd = os.path.dirname(os.path.realpath(__file__))
 sys.path.append(cd)
-sys.path.append('/code/cascadenik')
+sys.path.append(cd + '/../cascadenik')
+import cascadenik
 import TileStache
 from TileStache import Goodies
 from TileStache.Goodies import ExternalConfigServer
 
 #application = TileStache.WSGITileServer(cd + '/tilestache.cfg', False)
-application = ExternalConfigServer.WSGIServer("http://localhost:3000/config/tileserver")
+application = ExternalConfigServer.WSGIServer("http://localhost/config/tileserver")
 
